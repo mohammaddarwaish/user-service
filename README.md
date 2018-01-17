97b24bd919dad9504a499267eebc39576af47d40
Mohammad Darwaish <darwaishsm@gmail.com>
2018-01-17 21:08:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# user-service
+Provide User CRUD, authorisation and authentication operations
---
+++ src/main/java/mohammaddarwaish/com/github/userservice/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package mohammaddarwaish.com.github.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+}
